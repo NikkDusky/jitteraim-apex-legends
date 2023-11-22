44d04cd490f9a3d208ec097f0ebfa61e4a075038
Nikk <nikkdusky@gmail.com>
2023-11-22 07:19:29 +0500

    Add files via upload

---
+++ SHEESH v2.py
@@ -0,0 +1,36 @@
+from win32api import GetKeyState, mouse_event
+from time import sleep
+
+PLUS = 5
+MINUS = -5
+CODE = 0x01
+
+def SLEEP(timing=0.00001):
+    sleep(timing)
+
+def MSEVENT(plusPXL, minusPXL):
+    mouse_event(0x01, plusPXL, minusPXL)
+    
+def SQUARE():
+    while True:
+        if GetKeyState(CODE) < 0:
+            MSEVENT(PLUS, 0)
+            MSEVENT(PLUS, 0)
+            SLEEP()
+            
+            MSEVENT(0, MINUS)
+            MSEVENT(0, MINUS)
+            SLEEP()
+            
+            MSEVENT(MINUS, 0)
+            MSEVENT(MINUS, 0)
+            SLEEP()
+            
+            MSEVENT(0, PLUS)
+            MSEVENT(0, PLUS)
+        else:
+            continue
+
+if __name__ == "__main__":
+    print("Для выхода нажми CTRL + C\nИли просто закрой это окно =)")
+    SQUARE()
